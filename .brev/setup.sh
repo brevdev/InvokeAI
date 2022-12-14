e405e16d856f1532165035aa5db054f2387896cd
samlhuillier1 <sam.jansa123@gmail.com>
2022-12-14 00:20:24 +0000

    setup

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -4,5 +4,6 @@ set -eo pipefail
 sudo apt install python3.9 -y
 sudo     apt install python3.9-venv -y
 chmod +x ./installer/install.sh.in
+sudo rm -rf /home/ubuntu/invokeai
 # ./installer/install.sh.in   
 # we need to check too, if we can run this with the default option set so that no input is required
