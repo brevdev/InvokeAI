99fd28f2d95971fe25103d946d009e9990030c48
samlhuillier1 <sam.jansa123@gmail.com>
2023-02-01 18:45:57 +0000

    extra install for invoke

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -5,5 +5,6 @@ sudo apt install python3.9 -y
 sudo     apt install python3.9-venv -y
 chmod +x ./installer/install.sh.in
 sudo rm -rf /home/ubuntu/invokeai
+sudo apt-get update && sudo apt-get install ffmpeg libsm6 libxext6  -y
 # ./installer/install.sh.in   
 # we need to check too, if we can run this with the default option set so that no input is required
